002f464443a227cb99613ac0e5833bdd05a19960
Landon Waddell <Landon.waddell@gmail.com>
2021-08-08 20:02:24 -0600

    config changes

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -30,10 +29,10 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
+
